c4ca031b9b4bffb7a693bdb466bb8e1f29484407
Thibaud Jenny <thibaud.jenny@gmail.com>
2020-10-28 15:02:58 +0100

    SFML install

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include "Controler/jeu.h"
 
-
-
+#include <SFML/Window.hpp>
 
 int main()
 {
+    sf::Window window(sf::VideoMode(800, 600), "My window");
 
-    lancerPartie(20, 20, 0.3);
+    // on fait tourner le programme jusqu'à ce que la fenêtre soit fermée
+    while (window.isOpen())
+    {
+        lancerPartie(20, 20, 0.3);
+
+        // on inspecte tous les évènements de la fenêtre qui ont été émis depuis la précédente itération
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // évènement "fermeture demandée" : on ferme la fenêtre
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
 
     return 0;
 }
